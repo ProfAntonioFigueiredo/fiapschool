ab47b2ba8f1c4d6b3ed9ad8ba270a9f70b76f8e8
Antonio Figueiredo <profantonio.figueiredo@fiap.com.br>
2025-02-06 15:22:24 -0300

    Resumo e Correção Aula 06-Fev 2025

---
+++ Aulas/Exercicio06022025.js
@@ -0,0 +1,10 @@
+// Declaração das variáveis com const (valores fixos)
+const valorsaldo = 100;
+const valordeposito = 150;
+const valorsaque = 125;
+
+// Cálculo do valor final
+const valorfinal = valorsaldo - valorsaque + valordeposito;
+
+// Exibindo o resultado
+console.log("Valor final:", valorfinal);
